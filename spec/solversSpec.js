e946e0d599e2180459b9db0b6ba94fd05b70fc4d
Hack Reactor Students <wvillanu89@gmail.com>
2014-06-11 23:23:50 -0700

    Implemented changes

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -4,7 +4,7 @@ describe('solvers', function() {
   describe('findNRooksSolution()', function() {
 
     it('finds a valid solution for n of 1-8', function() {
-      _.range(1, 9).map(function(n) {
+      _.range(2, 9).map(function(n) {
         var solutionBoard = new Board(findNRooksSolution(n));
 
         expect(solutionBoard.get('n')).to.equal(n);
@@ -17,7 +17,7 @@ describe('solvers', function() {
   describe('countNRooksSolutions()', function() {
 
     it('finds the number of valid solutions for n of 1-8', function() {
-      _.range(1, 9).map(function(n) {
+      _.range(2, 9).map(function(n) {
         var solutionCount = countNRooksSolutions(n);
         var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n];
 
--- src/solvers.js
+++ src/solvers.js
@@ -14,8 +14,49 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  var solution = []; //fixme
+  var top = n;
+  ///PSEUDOCODIN'!!!!!
+  // Find an available spot for rook #1
+  var board = new Board({n:n});
+  var emptyBoard = board.rows();
+  var checkSolutions = function(n,boardLayout,conflictedRow,conflictedCol){
+    if (n===2){
+      debugger;
+    }
+    var conflictedRow = conflictedRow || {};
+    var conflictedCol = conflictedCol || {};
 
+    for (var i = 0; i < top; i ++){
+      if(!conflictedRow[i]){
+
+      // find first available column
+        for (var j=0; j < top; j++) {
+          var tempBoardLayout = boardLayout.slice();
+          if (!conflictedCol[j]){
+            conflictedRow[i]=true;
+            conflictedCol[j]=true;
+            tempBoardLayout[i][j] = 1;
+            if (n!== 1){
+              checkSolutions(n-1, tempBoardLayout,conflictedRow,conflictedCol);
+              delete conflictedRow[i];
+              delete conflictedCol[j];
+
+              solution.push(tempBoardLayout);
+            }
+          }
+        }
+      }
+    }
+  };
+  checkSolutions(n,emptyBoard);
+
+  // Save the rook's row and column as forbidden
+    // Find available spot for rook #2 that is not forbidden
+    // Repeat until we're out of rooks
+    //
+    // LOOKS LIKE RECURSION!!!
+    //
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
